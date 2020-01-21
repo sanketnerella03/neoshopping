ff012d8d2467753a500bdb2c25d7a2777b890549
Sanket Nerella <Sanket@bbd.co.za>
2020-01-21 18:16:25 +0530

    Shop Page : In Progress

---
+++ src/pages/shoppage/shoppage.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {SHOP_DATA} from  './shop.data';
+
+class ShopPage extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+    render(){
+        return (
+            <div className='shop-page'>
+                {
+                    this.state.collections.map(
+                        ({id, ...otherCollectionProps}) => (
+                         <CollectionPreview key={id} {...otherCollectionProps}/>   
+                        )
+                    )
+                }
+            </div>
+        );
+    }
+}
+
+export default ShopPage;
